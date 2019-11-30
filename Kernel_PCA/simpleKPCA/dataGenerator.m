6a75dba48da414424e37245757e2576442c0b8ba
Niraj Mahajan <nirajmahajan007@gmail.com>
2019-11-30 20:17:13 +0530

    Seems fine

--- Kernel_PCA/simpleKPCA/dataGenerator.m
+++ Kernel_PCA/simpleKPCA/dataGenerator.m
@@ -3,29 +3,29 @@
 function [Data] = dataGenerator(draw, sampleSize)
     %% Declare constants
     % Generate circular data of three different radii
-    % r = 0, 3, 6
+    % r = 1, 5, 9
     Data2 = zeros(sampleSize,2);
     Data5 = zeros(sampleSize,2);
     Data8 = zeros(sampleSize,2);
-    r1 = 0;
-    r2 = 3;
-    r3 = 6;
+    r1 = 1;
+    r2 = 5;
+    r3 = 9;
     
     %% Generate data
     % Generate 200 points each by uniformly drawing theta values
     for i = 1:sampleSize
         ang1 = 2*pi*rand();
-        rad1 = r1 + randn()/10;
+        rad1 = r1 + randn()/5;
         Data2(i,1) = rad1*cos(ang1);
         Data2(i,2) = rad1*sin(ang1);
 
         ang2 = 2*pi*rand();
-        rad2 = r2 + randn()/10;
+        rad2 = r2 + randn()/5;
         Data5(i,1) = rad2*cos(ang2);
         Data5(i,2) = rad2*sin(ang2);
 
         ang3 = 2*pi*rand();
-        rad3 = r3 + randn()/10;
+        rad3 = r3 + randn()/5;
         Data8(i,1) = rad3*cos(ang3);
         Data8(i,2) = rad3*sin(ang3);
     end
--- Kernel_PCA/simpleKPCA/projectData.m
+++ Kernel_PCA/simpleKPCA/projectData.m
@@ -2,9 +2,7 @@
 function [Y] = projectData(EV, Kernel, limit)
     N = size(Kernel, 1);
     Y = zeros(N, limit);
-    for i = [1:N]
-        size(EV)
-        size(Kernel(:, i))
+    for i = 1:N
         Y(i, :) = (EV' * Kernel(:, i))';
     end
 end
